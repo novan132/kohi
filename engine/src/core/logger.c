fbfbb96fbdb09e757e64f51dee304deeecfda96f
Novan Ariadi <l4novan132@gmail.com>
2024-08-14 12:00:53 +0700

    create logger module

---
+++ engine/src/core/logger.c
@@ -0,0 +1,36 @@
+#include "logger.h"
+
+// TODO: temporary
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+
+
+b8 initialize_logging() {
+    // TODO: create log file.
+    return TRUE;
+}
+
+void shutdown_logging() {
+    // TODO: cleanup logging/write queued entries.
+}
+
+KAPI void log_ouput(log_level level, const char *message, ...) {
+    const char* level_strings[6] = {"[FATAL]: ", "[ERROR]: ", "[WARN]: ", "[INFO]: ", "[DEBUG]: ", "[TRACE]: "};
+    // b8 is_error = level < 2;
+
+    // Technically impose a 32k character limit on single log entry
+    char out_message[32000];
+    memset(out_message, 0, sizeof(out_message));
+
+    __builtin_va_list arg_ptr;
+    va_start(arg_ptr, message);
+    vsnprintf(out_message, 32000, message, arg_ptr);
+    va_end(arg_ptr);
+
+    char out_message2[32000];
+    sprintf(out_message2, "%s%s\n", level_strings[level], out_message);
+
+    // TODO: platform specific output.
+    printf("%s", out_message2);
+}
--- testbed/src/main.c
+++ testbed/src/main.c
@@ -1,6 +1,11 @@
-#include <test.h>
+#include <core/logger.h>
 
 int main(void) {
-    print_int(42);
+    KFATAL("a test message: %f", 3.14f);
+    KERROR("a test message: %f", 3.14f);
+    KWARN("a test message: %f", 3.14f);
+    KINFO("a test message: %f", 3.14f);
+    KDEBUG("a test message: %f", 3.14f);
+    KTRACE("a test message: %f", 3.14f);
     return 0;
 }
